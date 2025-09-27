5a78c00450d547759b81e66f5ccaf5a2ddb3d939
Lulakshi madubashini <lulakshimadubashini@gmail.com>
2025-09-27 21:32:54 +0530

    3

---
+++ location/location.js
@@ -0,0 +1,6 @@
+// You can add interactive features later.
+// Example: Alert when clicking on map info
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Location Page Loaded ✅");
+});
